53cc1ae2665cd210f114d2dd92ea958e2c2bb55c
Derrick Strong <derrickmstrong@gmail.com>
2020-10-25 07:21:55 -0500

    Updates

---
+++ precache-manifest.eee558eb05d71cf34ace7b23f945da86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75240f183413db52ececafa63d90c4c7",
+    "url": "/reacttictactoe_explained/index.html"
+  },
+  {
+    "revision": "653222300699e7a57c7f",
+    "url": "/reacttictactoe_explained/static/css/main.3616bd68.chunk.css"
+  },
+  {
+    "revision": "dfc87d89017b4f9e91fc",
+    "url": "/reacttictactoe_explained/static/js/2.86a1717c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttictactoe_explained/static/js/2.86a1717c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653222300699e7a57c7f",
+    "url": "/reacttictactoe_explained/static/js/main.c9a85906.chunk.js"
+  },
+  {
+    "revision": "0e4106a4b9fa860dfd23",
+    "url": "/reacttictactoe_explained/static/js/runtime-main.1b3bbc38.js"
+  }
+]);
